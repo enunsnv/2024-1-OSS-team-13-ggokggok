7b1387ad968271e196dbd8eecaca7006e519f354
enunsnv <seoej0315@naver.com>
2024-06-07 15:42:39 +0900

    build...

--- frontend/ggok/src/pages/region/search-region.jsx
+++ frontend/ggok/src/pages/region/search-region.jsx
@@ -195,44 +195,50 @@ export default function SearchPlace() {
 
   const nav = useNavigate();
 
-const handleSearch = async () => {
-  setError(''); // 검색을 시작할 때 에러 메시지를 초기화
-
-  try {
-    const display = 5;
-    const start = 1;
-    const query = encodeURIComponent(searchTerm);
-    const api_url = `/v1/search/local.json?query=${query}&display=${display}&start=${start}`;
-
-    console.log('API URL:', api_url); // 요청 URL 확인
-
-    const response = await fetch(api_url, {
-      headers: {
-        'X-Naver-Client-Id': import.meta.env.VITE_NAVER_CLIENT_ID,
-        'X-Naver-Client-Secret': import.meta.env.VITE_NAVER_CLIENT_SECRET,
-      },
-    });
-
-    console.log('Response status:', response.status); // 응답 상태 확인
-
-    if (!response.ok) {
-      throw new Error('네이버 API 요청에 실패했습니다.');
+  const handleSearch = async () => {
+    setError(''); // 검색을 시작할 때 에러 메시지를 초기화
+  
+    try {
+      const display = 5;
+      const start = 1;
+      const query = encodeURIComponent(searchTerm);
+      const api_url = `/v1/search/local.json?query=${query}&display=${display}&start=${start}`;
+  
+      console.log('API URL:', api_url); // 요청 URL 확인
+  
+      const response = await fetch(api_url, {
+        headers: {
+          'X-Naver-Client-Id': import.meta.env.VITE_NAVER_CLIENT_ID,
+          'X-Naver-Client-Secret': import.meta.env.VITE_NAVER_CLIENT_SECRET,
+        },
+      });
+  
+      console.log('Response status:', response.status); // 응답 상태 확인
+  
+      if (!response.ok) {
+        throw new Error('네이버 API 요청에 실패했습니다.');
+      }
+  
+      let data;
+      try {
+        data = await response.json();
+        console.log('Response data:', data); // 응답 데이터 확인
+      } catch (error) {
+        // JSON 파싱에 실패할 경우에 대비하여 적절한 처리를 수행합니다.
+        throw new Error('서버에서 온 데이터를 JSON으로 파싱하는데 실패했습니다.');
+      }
+  
+      if (data && data.items && data.items.length === 0) {
+        setError('검색 결과가 없습니다.');
+      } else {
+        setSearchResult(data);
+      }
+    } catch (error) {
+      console.error(error);
+      setError('검색어를 다시 입력해주세요.');
     }
-
-    const data = await response.json();
-
-    console.log('Response data:', data); // 응답 데이터 확인
-
-    if (data && data.items && data.items.length === 0) {
-      setError('검색 결과가 없습니다.');
-    } else {
-      setSearchResult(data);
-    }
-  } catch (error) {
-    console.error(error);
-    setError('검색어를 다시 입력해주세요.');
-  }
-};
+  };
+  
   
   
 
