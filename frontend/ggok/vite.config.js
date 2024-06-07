0e9d4842a71a301b28195ad16ae33c3edee8571a
enunsnv <seoej0315@naver.com>
2024-06-07 12:50:30 +0900

    build

--- frontend/ggok/vite.config.js
+++ frontend/ggok/vite.config.js
@@ -1,27 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables from .env files
-  const env = loadEnv(mode, process.cwd(), '');
-
-  console.log('Environment Variables:', env);
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/v1': {
-          target: 'https://openapi.naver.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/v1/, '/v1')
-        },
-        '/:path*': {
-          target: 'https://openapi.naver.com',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/(.+)/, '/$1')
-        }
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build', // 출력 디렉토리를 'build'로 설정
+  },
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'https://openapi.naver.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/v1/, '/v1')
       }
     }
-  };
+  }
 });
